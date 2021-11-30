576440dd8545fc78e1d5c0cf08c69a551d3433c4
Dudeful <rellumnyar@gmail.com>
2021-11-30 09:26:57 -0300

    finish basic structure

--- Kenji/JavaScript/aula-12/app.js
+++ Kenji/JavaScript/aula-12/app.js
@@ -1,3 +1,18 @@
-document.getElementById('container').innerHTML = `
-  <h1>hellofriend</h1>
-`;
+const write_phrases = () => {
+  document.getElementById('blackboard').innerHTML = '';
+
+  let blackboardWidth = window.innerWidth * 0.74;
+  let x = blackboardWidth / 245;
+
+  console.log(x);
+
+  let i = 0;
+  const nPhrases = document.getElementById('n_phrases').value;
+
+  while (i < nPhrases) {
+    document.getElementById('blackboard').innerHTML += `
+      <span class="bart_word">A verdade não está la fora</span>
+    `;
+    i++;
+  }
+};
